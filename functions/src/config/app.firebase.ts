5d7a8231c382461b2b526907c93112193b5fd4a6
IDGS-803-22002362 <81542@alumnos.utleon.edu.mx>
2026-01-27 16:02:21 -0600

    fix8

--- functions/src/config/app.firebase.ts
+++ functions/src/config/app.firebase.ts
@@ -1,18 +1,44 @@
 import { admin } from "./firebase.admin";
 import { getFirestore } from "firebase-admin/firestore";
+import * as path from "path";
+import * as fs from "fs";
 
-const serviceAccount = require("../../../serviceAccountAppOficial.json");
+let serviceAccount;
 
-let appOficial = admin.apps.find(app => app?.name === "APP_OFICIAL");
+// Detectar si estamos en Cloud Functions
+const isCloudFunction = process.env.FUNCTION_NAME || process.env.K_SERVICE;
+
+if (!isCloudFunction) {
+  // Solo cargar service account en desarrollo local o CI/CD
+  const serviceAccountPath = path.join(
+    __dirname,
+    "../../../serviceAccountAppOficial.json",
+  );
+
+  if (fs.existsSync(serviceAccountPath)) {
+    serviceAccount = require(serviceAccountPath);
+  } else if (process.env.SERVICE_ACCOUNT_APP_OFICIAL) {
+    serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_APP_OFICIAL);
+  } else {
+    throw new Error(
+      "No se encontró serviceAccountAppOficial.json ni SERVICE_ACCOUNT_APP_OFICIAL",
+    );
+  }
+}
+
+let appOficial = admin.apps.find((app) => app?.name === "APP_OFICIAL");
 
 if (!appOficial) {
-    appOficial = admin.initializeApp(
-        {
-            credential: admin.credential.cert(serviceAccount),
-            projectId: "app-oficial-leon",
-        },
-        "APP_OFICIAL"
-    );
+  const config: any = {
+    projectId: "app-oficial-leon",
+  };
+
+  // Solo agregar credenciales si no estamos en Cloud Functions
+  if (!isCloudFunction && serviceAccount) {
+    config.credential = admin.credential.cert(serviceAccount);
+  }
+
+  appOficial = admin.initializeApp(config, "APP_OFICIAL");
 }
 
 export const firestoreApp = getFirestore(appOficial);
