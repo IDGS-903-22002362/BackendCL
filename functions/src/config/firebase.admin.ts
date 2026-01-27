2731550201f29d257f1acf0ed2f3562ad05f4842
IDGS-803-22002362 <81542@alumnos.utleon.edu.mx>
2026-01-27 15:51:22 -0600

    fix6

--- functions/src/config/firebase.admin.ts
+++ functions/src/config/firebase.admin.ts
@@ -1,10 +1,27 @@
 // config/firebase.admin.ts
 import * as admin from "firebase-admin";
+import * as path from "path";
+import * as fs from "fs";
 
 if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(
-    process.env.SERVICE_ACCOUNT_APP_OFICIAL as string,
+  let serviceAccount;
+
+  // Ruta al archivo en el workspace (GitHub Actions lo crea aquí)
+  const serviceAccountPath = path.join(
+    __dirname,
+    "../../../serviceAccountAppOficial.json",
   );
+
+  // Prioridad: archivo físico > variable de entorno
+  if (fs.existsSync(serviceAccountPath)) {
+    serviceAccount = require(serviceAccountPath);
+  } else if (process.env.SERVICE_ACCOUNT_APP_OFICIAL) {
+    serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_APP_OFICIAL);
+  } else {
+    throw new Error(
+      "No se encontró serviceAccountAppOficial.json ni SERVICE_ACCOUNT_APP_OFICIAL",
+    );
+  }
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
--- functions/src/config/firebase.ts
+++ functions/src/config/firebase.ts
@@ -1,7 +1,25 @@
 import { admin } from "./firebase.admin";
 import { getFirestore } from "firebase-admin/firestore";
+import * as path from "path";
+import * as fs from "fs";
 
-const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY as string);
+let serviceAccount;
+
+const serviceAccountPath = path.join(
+  __dirname,
+  "../../../serviceAccountKey.json",
+);
+
+// Prioridad: archivo físico > variable de entorno
+if (fs.existsSync(serviceAccountPath)) {
+  serviceAccount = require(serviceAccountPath);
+} else if (process.env.SERVICE_ACCOUNT_KEY) {
+  serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
+} else {
+  throw new Error(
+    "No se encontró serviceAccountKey.json ni SERVICE_ACCOUNT_KEY",
+  );
+}
 
 let tiendaApp = admin.apps.find((app) => app?.name === "TIENDA_APP");
 
