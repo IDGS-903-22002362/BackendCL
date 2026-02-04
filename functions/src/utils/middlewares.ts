5cd7a5b583ef52b2fe187b87f113c930ac227534
IDGS-803-22001034 <80085@alumnos.utleon.edu.mx>
2026-02-04 14:09:27 -0600

    Merge completado y revisado

--- functions/src/utils/middlewares.ts
+++ functions/src/utils/middlewares.ts
@@ -2,16 +2,21 @@ import { firestoreApp } from "../config/app.firebase";
 import { admin } from "../config/firebase.admin";
 import { Request, Response, NextFunction } from "express";
 
-export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+export const authMiddleware = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
 
     const token = req.headers.authorization?.split("Bearer ")[1];
 
-    if (!token) return res.status(401).json({ message: "No autorizado" });
+    if (!token) {
+        res.status(401).json({ message: "No autorizado" });
+        return;
+    }
 
     try {
-
         const decoded = await admin.auth().verifyIdToken(token);
-
 
         const snapshot = await firestoreApp
             .collection("usuariosApp")
@@ -20,7 +25,8 @@ export const authMiddleware = async (req: Request, res: Response, next: NextFunc
             .get();
 
         if (snapshot.empty) {
-            return res.status(404).json({ message: "Usuario no registrado" });
+            res.status(404).json({ message: "Usuario no registrado" });
+            return;
         }
 
         req.user = {
@@ -29,8 +35,10 @@ export const authMiddleware = async (req: Request, res: Response, next: NextFunc
         };
 
         next();
+        return;
 
     } catch {
-        return res.status(401).json({ message: "Token inválido" });
+        res.status(401).json({ message: "Token inválido" });
+        return;
     }
 };
