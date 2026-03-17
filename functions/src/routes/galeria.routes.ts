feec97bf766e9d946bf53335b259ed804160bc1e
IDGS-803-22001034 <80085@alumnos.utleon.edu.mx>
2026-03-17 12:45:31 -0600

    galeria y funcionalidades extra v3

--- functions/src/routes/galeria.routes.ts
+++ functions/src/routes/galeria.routes.ts
@@ -15,7 +15,7 @@ const router = Router();
 const upload = multer({
     storage: multer.memoryStorage(),
     limits: {
-        fileSize: 20 * 1024 * 1024, // 20MB
+        fileSize: 100 * 1024 * 1024, // 20MB
     },
 });
 
